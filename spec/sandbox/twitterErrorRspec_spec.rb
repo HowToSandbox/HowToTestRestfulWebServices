d0d08fecf359d8b21e9b6b0080d62e8394087eb8
Frances Morales <fmorales@francesmorales.com>
2014-09-25 16:44:33 -0700

    Add comments to explain what is going on

--- spec/sandbox/twitterErrorRspec_spec.rb
+++ spec/sandbox/twitterErrorRspec_spec.rb
@@ -7,9 +7,9 @@ describe 'twitter error' do
 			get 'https://api.twitter.com/1.1/statuses/home_timeline.json'
 			puts "JSON: #{response}"  # {"errors":[{"message":"Bad Authentication data","code":215}]}
 			
-			expect_json_types({errors: :array})
-			expect_json_types('errors.0', {message: :string, code: :int})
-			expect_json('errors.0', {message: "Bad Authentication data", code: 215})
+			expect_json_types({errors: :array})												# Check that the type of errors: is :array
+			expect_json_types('errors.*', {message: :string, code: :int})					# Check all the types in the Array 'errors'
+			expect_json('errors.0', {message: "Bad Authentication data", code: 215})		# Check the data in the first element of the array 'errors'
 		end
 	puts "rspec ran"
 end
