133e9e3ad370688e3645ec7e2513ba4560fa28f1
Frances Morales <fmorales@francesmorales.com>
2014-10-06 11:46:56 -0700

    I finally got some airborne stuff working. I’ll just put this here for now before I shuffle the code around someplace permanant

--- spec/sandbox/helloWorldRspec_spec.rb
+++ spec/sandbox/helloWorldRspec_spec.rb
@@ -1,45 +1,37 @@
 require 'rspec'	
 require 'airborne'
-#require 'json'
 
-describe 'twiddling apis for fun and profit' do
+describe 'Tickling APIs for fun - ' do
 
-		# it 'should validate the type on an http url' do
+		# it 'Tickle the Caltech Calendar API' do
 		# 	get 'http://www.caltech.edu/calendar_api/list.json?sponsor_ids=8957'
-		# 	puts response
+		# 	#puts response
 		# 	expect_json({"id" => "32057"})
 		# end
 
-		it 'should be able to give me something from twitter' do
+		it 'Tickle the Twitter API' do
 			get 'https://api.twitter.com/1.1/statuses/home_timeline.json'
-			puts "JSON: #{response}"  # {"errors":[{"message":"Bad Authentication data","code":215}]}
+			#puts "Twitter: #{response}"  # {"errors":[{"message":"Bad Authentication data","code":215}]}
 			
 			expect_json_types({errors: :array})
-			expect_json_types('errors.0', {message: :string, code: :int})
+			expect_json_types('errors.*', {message: :string, code: :int})
 			expect_json('errors.0', {message: "Bad Authentication data", code: 215})
 		end
 
-  # it 'should validate types' do
-  #   get 'http://example.com/api/v1/simple_get' #json api that returns { "name" : "John Doe" } 
-  #   expect_json_types({name: :string})
-  # end
-		# it 'should validate types' do
-		# 	#get 'http://example.com/api/v1/simple_get' #this works
-			
-		# 	#expect_json_types({name: :string})
-		# 	#get 'https://zkillboard.com/api/solo/kills/characterID/268946627/' #this does not
-		# 	get 'http://www.caltech.edu/calendar_api/list.json?sponsor_ids=8957'
-		# 	puts response
-		# 	#expect_json_types({:killID => :string})
-		# 	# expect_json_types({:victim => :array})
-		# 	# puts "The things are types are validated"
-		# 	# puts "test1 ran"
+		it "Tickle the zKillboard API" do
+			get 'https://zkillboard.com/api/solo/kills/characterID/268946627/'
+			puts "zKillboard is: #{json_body[1]}"
+			#puts json_body[1][:victim].keys
+			expect_json_types('*.victim', {corporationName: :string}) #killID: :string, 
+		end
+
+		# it "Tickle for a single item " do
+		# 	get 'https://zkillboard.com/api/solo/kills/characterID/268946627/'
+		# 	puts "zKillboard is: #{json_body[1]}"
+		# 	expect_json_types('*', {moonID: :string}) # This should pass
+		# 	expect_json_types('*', {moonID: :int})    # This should fail because you get a string 
+		# 	# This is failing because of bad data. Some data is strings, some is int. Bad Database! Bad!
 		# end
 
-		# it 'should validate values' do
-		# 	get 'https://zkillboard.com/api/solo/kills/characterID/268946627/'
-		# 	expect_json({:killID => "26926124"})
-		# 	puts 'test2 ran'
-		# end
-	puts "rspec ran"
+	
 end
